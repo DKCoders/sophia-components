a4f81914798eca1219c9afd345da772c8ca9ef2f
alexys <alexysbike@gmail.com>
2018-05-04 17:30:31 -0300

    WIP stoybook

--- lib/components/Button/Button.jsx
+++ lib/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ content, id }) => (
-  <button>{content}</button>
+const Button = ({ content }) => (
+  <button className="button">{content}</button>
 );
 
 Button.propTypes = {
