b1cd6458dac2b6f3331ca29d9d3ddf7c510b1155
Alexys Gonzalez <alexysbike@gmail.com>
2018-06-17 20:43:54 -0400

    Changing default events

--- lib/elements/Icon.jsx
+++ lib/elements/Icon.jsx
@@ -4,9 +4,10 @@ import compose from 'recompose/compose';
 import withAttrs from '../base/withAttrs';
 import withIsHas, { helpersIsKeys, helpersHasKeys, sizeKeys } from '../base/withIsHas';
 import { classNameJoiner, combineSets } from '../utils/helpers';
+import withEvents from "../base/withEvents";
 
-const Icon = ({ icon, attrs: { className, ...restAttrs } }) => (
-  <span className={classNameJoiner('icon', className)} {...restAttrs}>
+const Icon = ({ icon, attrs: { className, ...restAttrs }, events }) => (
+  <span className={classNameJoiner('icon', className)} {...restAttrs} {...events}>
     <i className={icon} />
   </span>
 );
@@ -14,11 +15,13 @@ const Icon = ({ icon, attrs: { className, ...restAttrs } }) => (
 Icon.propTypes = {
   icon: PropTypes.string.isRequired,
   attrs: PropTypes.shape().isRequired,
+  events: PropTypes.shape().isRequired,
 };
 
 Icon.defaultProps = {};
 
 export default compose(
+  withEvents(),
   withIsHas(combineSets(helpersIsKeys, sizeKeys, ['left', 'right']), helpersHasKeys),
   withAttrs(),
 )(Icon);
